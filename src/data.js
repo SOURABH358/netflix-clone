0ec37136d570f4ac831b92201332d53d17862daa
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-15 20:13:28 +0530

    added files

--- src/data.js
+++ src/data.js
@@ -1,5 +1,51 @@
 export default Netflix__data = {
+    cards: [
+        {
+            title: 'Enjoy on your TV.',
+            info: 'Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.',
+            image: ''
+        },
+        {
+            title: 'Download your shows to watch offline.',
+            info: 'Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.',
+            image: ''
+        },
+        {
+            title: 'Watch everywhere.',
+            info: 'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.',
+            image: ''
+        },
+        {
+            title: 'Create profiles for children.',
+            info: 'Send children on adventures with their favourite characters in a space made just for them—free with your membership.',
+            image: ''
+        }
+
+    ],
     faq: [
-        {question: 'What is Netflix?'}
+        {
+            question: 'What is Netflix?',
+            answer: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices. \nYou can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!"
+        },
+        {
+            question: "How much does Netflix cost?",
+            answer: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts."
+        },
+        {
+            question: "What can I watch?",
+            answer: "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. \nYou can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere."
+        },
+        {
+            question: "How do I cancel?",
+            answer: "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime."
+        },
+        {
+            question: "What can I watch on Netflix?",
+            answer: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want."
+        },
+        {
+            question: "Is Netflix good for kids?",
+            answer: "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space. \nKids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see."
+        }
     ]
 }
