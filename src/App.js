42738dc4ab73c9e23db77d27aabb2ae11367674c
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-15 20:03:18 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Banner from "./Banner";
+import Home from "./Home";
 import SignIn from "./SignIn";
 export default function App(){
     return (
         <>
             
             <Routes>
-                <Route path="/" element = {<Banner/>}/>
+                <Route path="/" element = {<Home/>}/>
                 <Route path = "/sign-in" element={<SignIn/>}/>
             </Routes>
         </>
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Banner from "./Banner";
+function Home(){
+    return<>
+        <Banner/>
+    </>
+}
+
+export default Home
