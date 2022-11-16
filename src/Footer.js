680b30f4a3ad6c80bd56bc876edc45e3c0a61ca9
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-16 12:57:23 +0530

    added files

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <section className="footer__section">
+        <p className="footer__title">Frequently Asked Questions</p>
+    </section>
+}
+
+export default Footer
