d7dcd34c262034f71db811d4feaf7018f896c073
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-17 12:39:42 +0530

    added files

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "./components/Banner";
 import Cards from "./components/Cards";
 import Faq from "./components/Faq"
+import Footer from "./components/Footer"
 function Home(){
 
     return<>
         <Banner/>
         <Cards/>
         <Faq/>
+        <Footer/>
     </>
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+// eslint-disable
+import './Footer.css'
+function Footer() {
+    return <section className='footer__section'>
+        <p className='footer__title'>
+            Questions? Call <a href="https://www.netflix.com/in/">000-800-040-1843</a>
+        </p>
+        <div className='footer__links'>
+                <div><a href='https://www.netflix.com/in/'>FAQ</a></div>
+                <div><a href='https://www.netflix.com/in/'>Help Centre</a></div>
+                <div><a href='https://www.netflix.com/in/'>Account</a></div>
+                <div><a href='https://www.netflix.com/in/'>Media Centre</a></div>
+                <div><a href='https://www.netflix.com/in/'>Investor Relations</a></div>
+                <div><a href='https://www.netflix.com/in/'>Jobs</a></div>
+                <div><a href='https://www.netflix.com/in/'>Ways to watch</a></div>
+                <div><a href='https://www.netflix.com/in/'>Terms of Use</a></div>
+                <div><a href='https://www.netflix.com/in/'>Privacy</a></div>
+                <div><a href='https://www.netflix.com/in/'>Cookie Preferences</a></div>
+                <div><a href='https://www.netflix.com/in/'>Information</a></div>
+                <div><a href='https://www.netflix.com/in/'>Contact</a></div>
+                <div><a href='https://www.netflix.com/in/'>Speed Test</a></div>
+                <div><a href='https://www.netflix.com/in/'>Legal Notices</a></div>
+                <div><a href='https://www.netflix.com/in/'>Only on Netflix</a></div>
+        </div>
+    </section>
+}
+
+export default Footer;
