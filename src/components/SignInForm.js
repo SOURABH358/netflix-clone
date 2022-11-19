723e2c1ed3bf3f1796d25ca0b9f278f4aff42b46
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-19 12:22:14 +0530

    added files

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -11,60 +11,68 @@ function SignInForm() {
     return <div className="SignIn__form">
         <CssBaseline />
         <ThemeProvider theme={theme}>
-            <Typography component="h1" variant="h4" 
-            style={{
-                color: "#ffffff"
-            }}>
+            <Typography component="h1" variant="h4"
+                style={{
+                    color: "#ffffff"
+                }}>
                 Sign In
             </Typography>
-            <Box component="form" sx={{mt:4}}>
-                <TextField 
-                style={{
-                    background: "#303030",
-                    borderRadius: "8px",
-                }}
-                 margin="normal"
-                 required
-                 fullWidth
-                 id="email"
-                 label="Email Address"
-                 name="email"
-                 autoComplete="email"
-                 autoFocus
+            <Box component="form" sx={{ mt: 4 }}>
+                <TextField
+                    inputProps={{ style: { color: 'white' } }}
+                    InputLabelProps={{
+                        style: { color: 'grey' }
+                    }}
+                    style={{
+                        background: "#303030",
+                        borderRadius: "8px",
+                    }}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="filled-search"
+                    label="Email or Phone number"
+                    type="email"
+                    variant="filled"
+                    autoComplete="email"
+                    autoFocus
                 />
-                 <TextField
-                 inputProps={{style:{color: 'white'}}}
-                 sx={{
-                    background: "#303030",
-                    borderRadius: "8px",
-                    
-                }}
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"/>
-                  
-                  <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="error"
-                  sx={{ mt: 4, mb: 2, py:2,}}
-                  >Sign in</Button>
-                  <FormControlLabel
-                  sx={{
-                    color: "white"
-                  }}
-                  control={<Checkbox value="remember" sx={{
-                    color: "white"
-                  }}/>}
-                  label = "Remember Me"/>
+                <TextField
+                    inputProps={{ style: { color: 'white' } }}
+                    InputLabelProps={{
+                        style: { color: 'grey' }
+                    }}
+                    sx={{
+                        background: "#303030",
+                        borderRadius: "8px",
+
+                    }}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="filled-search"
+                    label="Password"
+                    type="password"
+                    variant="filled"
+                    autoComplete="current-password" />
+
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="error"
+                    sx={{ mt: 4, mb: 2, py: 2, }}
+                >Sign in</Button>
+                <FormControlLabel
+                    sx={{
+                        color: "grey"
+                    }}
+                    control={<Checkbox value="remember" sx={{
+                        color: "grey"
+                    }} />}
+                    label="Remember Me" />
             </Box>
-            <Box sx={{mt:2}}>
+            <Box sx={{ mt: 2 }}>
                 <Typography variant="p" color={greyColor}>New to Netflix? </Typography>
                 <Link to="/">sign up</Link>
             </Box>
