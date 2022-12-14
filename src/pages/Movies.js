805bf7e06138183a8df11551bd51fa8e5611d8a4
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-14 12:15:43 +0530

    added files

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -4,6 +4,7 @@ import MovieRow from "../components/MovieRow.js";
 import axios from "axios";
 import { movieGenre } from "../data"
 import Modal from "../components/Modal.js";
+import ReactPlayer from "react-player";
 const Movies = () => {
     const [modal, setModal] = useState(false);
     const [currentMovie, setCurrentMovie] = useState(null);
@@ -37,28 +38,26 @@ const Movies = () => {
             </div>
             <div className="movie__jumbotron">
                 <div className="movie__content">
-                <h3>{movieData.length?movieData[0].movieList[14].title:""}</h3>
-                <p style={{ color: "var(--lighter-grey)" }}><span>{movieData.length?movieData[0].movieList[14].release_date.slice(0,4):""}</span>|
-                    <span>{movieData.length?movieData[0].movieList[14].adult?"U/A 16+":"U/A 13+":""}</span>|
+                <h3>Avatar: The Way of Water</h3>
+                <p style={{ color: "var(--lighter-grey)" }}><span>2022</span>|
+                    <span>U/A 13+</span>|
                     <span>2h 27min</span>|
-                    <span>{movieData.length?movieData[0].movieList[14].genre_ids.map((el)=>{
-        return movieGenre.filter(item=>item.id === el)[0].name
-    }).join(", "):""}</span>
+                    <span>Science Fiction, Action & Adventure</span>
                 </p>
-                <p>{movieData.length?movieData[0].movieList[14].overview:""}</p>
+                <p>Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.</p>
                 <button className="watch__now">▶ Play</button>
                 <button className="trailer">More Info</button>
                 </div>
-                <div className="movie__hero"
-                // style={{
-                //     background: movieData.length?`url(https://image.tmdb.org/t/p/w500${movieData[0].movieList[0].backdrop_path})`:"",
-                //     backgroundPosition: "center",
-                //     backgroundSize: "cover",
-                //     backgroundRepeat: "no-repeat"
-                // }}
-                >
-                    {/* <img src={movieData.length?`https://image.tmdb.org/t/p/original${movieData[0].movieList[6].poster_path}`:""} alt="poster"/> */}
-                    <img src={movieData.length?`https://image.tmdb.org/t/p/w500${movieData[0].movieList[14].backdrop_path}`:""} alt="poster"/>
+                <div className="movie__hero">
+                    {/* <ReactPlayer className="movie__trailer" 
+                    url="https://youtu.be/d9MyW72ELq0"
+                    loop={true}
+                    playing={true}
+                    muted={true}
+                    width='100%'
+                    height='100%'/> */}
+                    <img src="/images/avatar__poster.jpg" alt="avatar__poster"/>
+                    
                     <div className="layover3"></div>
                 </div>
             </div>
